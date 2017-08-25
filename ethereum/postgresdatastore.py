d021d53ef8437de062f7856d0f18c207df4b7bfb
Dan Gailey <ournive@gmail.com>
2017-08-25 16:39:03 -0700

    updates that are now working to dump to the db

--- ethereum/postgresdatastore.py
+++ ethereum/postgresdatastore.py
@@ -3,14 +3,16 @@ import logging
 import psycopg2
 import sys
 import copy
+import codecs
+import string
 
 from datastore import Datastore
 
 
 class PostgresDatastore(Datastore):
 
-    TX_TABLE_NAME = "ethereum-transaction"
-    BLK_TABLE_NAME = "ethereum-block"
+    TX_TABLE_NAME = "ethereum_transactions"
+    BLK_TABLE_NAME = "ethereum_blocks"
 
     def __init__(self):
         super().__init__()
@@ -24,6 +26,7 @@ class PostgresDatastore(Datastore):
         self.dbname = dbname
         self.dbport = dbport
         self.dbhost = dbhost
+        print("Connecting to db using dbuser: %s, dbpass: %s, dbname: %s, dbport: %s, dbhost: %s" % (dbuser, dbpass, dbname, dbport, dbhost))
 
     def extract(self, rpc_block):
 
@@ -34,25 +37,27 @@ class PostgresDatastore(Datastore):
 
       block_number_int = int(block["number"], 0)
 
-      block_timestamp_int = datetime.datetime.fromtimestamp(int(block["timestamp"], 0))
+      block_timestamp_datetime = datetime.datetime.fromtimestamp(int(block["timestamp"], 0))
 
       for tx in transactions:
         tx["blockNumber_int"] = block_number_int
-        tx["blockTimestamp_int"] = block_timestamp_int
+        tx["blockTimestamp"] = block_timestamp_datetime
+        tx["blockTimestamp_int"] = block_timestamp_datetime
         # Convert wei into ether
         tx["value_eth"] = int(tx["value"], 0) / self.WEI_ETH_FACTOR
         tx["value_int"] = int(tx["value"], 0)
         tx["gasPrice_int"] = int(tx["gasPrice"], 0)
         tx["gas_int"] = int(tx["gas"], 0)
-        tx["input_str"] = str(tx["input"])
+        tx["input_str"] = self.hex_to_ascii(tx["input"])
         tx["transactionIndex_int"] = int(tx["transactionIndex"], 0)
         tx_value_sum += int(tx["value"], 0)
 
       block["transactions"] = transactions
       block["number_int"] = block_number_int
       block["difficulty_int"] = int(block["difficulty"], 0)
-      block["extraData_str"] = str(block["extraData"])
-      block["timestamp_int"] = block_timestamp_int
+      block["extraData_str"] = self.hex_to_ascii(block["extraData"])
+      block["timestamp"] = block_timestamp_datetime
+      block["timestamp_int"] = block_timestamp_datetime
       block["gasLimit_int"] = int(block["gasLimit"], 0)
       block["gasUsed_int"] = int(block["gasUsed"], 0)
       block["size_int"] = int(block["size"], 0)
@@ -60,60 +65,82 @@ class PostgresDatastore(Datastore):
       block["txValueSum_int"] = tx_value_sum # going to keep this in Wei
       block["totalDifficulty_int"] = int(block["totalDifficulty"], 0)
 
+      block["uncles"] = ''
+
       self.blocks.append(block)
 
 
     def save(self):
+      if not self.blocks:
+        print("No blocks found")
+        return True
+      else:
+        for block in self.blocks:
+          just_block = copy.deepcopy(block)
+          del just_block["transactions"]
+          transactions = block["transactions"]
 
-      if self.blocks:
-        try:
-          for block in self.blocks:
-            just_block = copy.deepcopy(block)
-            del just_block["transactions"]
-            transactions = block["transactions"]
-
-            with self.db_conn:
-              with self.db_conn.cursor() as dbcurs:
-                # if block present skip
-
+          with self.db_conn:
+            with self.db_conn.cursor() as dbcurs:
+              # if block present skip
+              try:
                 block_check_sql = 'select hash from ethereum_blocks where hash = %s'
                 dbcurs.execute(block_check_sql, (block['hash'],))
                 if dbcurs.fetchone() == None:
 
                   block_insert = 'insert into ethereum_blocks (%s) values %s'
                   l = [(c, v) for c, v in just_block.items()]
-                  block_columns = ','.join([t[0] for t in l])
+                  block_columns = ','.join([t[0] for t in l]).split(",")
+                  block_columns = ', '.join(['"{}"'.format(value) for value in block_columns]).lower()
+
                   block_values = tuple([t[1] for t in l])
 
                   block_sql = self.db_cursor.mogrify(block_insert, ([psycopg2.extensions.AsIs(block_columns)] + [block_values]))
+                  print("Write block %s" % block['number_int'])
 
                   dbcurs.execute(block_sql)
+              except Exception as exception:
+                print(exception)
+                print(block_columns)
+                print(block_values)
+                print("block!!!!!")
 
-                for tx in transactions:
-                  # check to see if TX exists
+              for tx in transactions:
+                # check to see if TX exists
+                try:
                   tx_check_sql = 'select hash from ethereum_transactions where hash = %s and blockHash = %s;'
                   dbcurs.execute(tx_check_sql, (tx['hash'], tx['blockHash'],))
 
                   if dbcurs.fetchone() == None:
                     tx_insert = 'insert into ethereum_transactions (%s) values %s'
-                    l = [(c, v) for c, v in transaction.items()]
-                    tx_columns = ','.join([t[0] for t in l])
+                    l = [(c, v) for c, v in tx.items()]
+                    tx_columns = ','.join([t[0] for t in l]).split(",")
+
+                    tx_columns = ', '.join(['"{}"'.format(value) for value in tx_columns]).lower()
                     tx_values = tuple([t[1] for t in l])
 
                     tx_sql = self.db_cursor.mogrify(tx_insert, ([psycopg2.extensions.AsIs(tx_columns)] + [tx_values]))
                     dbcurs.execute(tx_sql)
-                # Update state
-                update_sql = "update ethereum_parser_state where id=1 set (last_blockNumber) values (%s)"
-                dbcurs.execute(update_sql, (block['blockNumber_int'],))
+                    print("- Write tx %s" % tx['hash'])
+                except Exception as exception:
+                  print(tx_columns)
+                  print(tx_values)
+                  print(exception)
+                  print("tx!!!!!")
 
-          #return "{} blocks and {} transactions indexed".format(
-          #)
+              update_sql = "update ethereum_parser_states set last_block_number = %s where id = 1"
+              dbcurs.execute(update_sql, (block['number_int'],))
 
-        except Exception as exception:
-          print("Issue with {} blocks:\n{}\n".format(self.blocks, exception))
-          for block in self.blocks:
-              logging.error("block: " + str(block["number"]))
+      #except Exception as exception:
+      #  for block in self.blocks:
+      #      logging.error("block: " + str(block["number"]))
 
+    def hex_to_ascii(self, hex):
+      if hex == "0x":
+        return ''
+      else:
+        # not sure if this should be UTF-8
+        return codecs.encode(codecs.decode(codecs.decode(str.replace(hex, '0x', ''), "hex"), "ascii", "ignore"), "ascii", "ignore")
 
     @staticmethod
     def request(url, **kwargs):
--- ethereum/schema.sql
+++ ethereum/schema.sql
@@ -1,69 +1,126 @@
+/* Probably should have used this in retrospect: https://github.com/almindor/etherdb/blob/master/sql/tables.sql */
+
+/*
+{
+  'blockHash': '0x72d328af4835d1c087c39326317b5b98c70124760c3788ae1b1dd87736f19bd4', 
+  'blockNumber': '0x3bc50f', 
+  'from': '0x52bc44d5378309ee2abf1539bf71de1b7d7be3b5', 
+  'gas': '0xc350', 
+  'gasPrice': '0x12a05f200', 
+  'hash': '0xdc256d0fb22cad4c09bbb39b04a6d698fb3b4a2f48007b6b72c2914c4e49e133', 
+  'input': '0x', 
+  'nonce': '0x20b3c5', 
+  'to': '0x58e4dd01bdaae3b04d7f725ad7b39520b8c91df2', 
+  'transactionIndex': '0x51', 
+  'value': '0x2d1cf2b663599e0', 
+  'v': '0x26', 
+  'r': '0x1d4fccde10ef2f2603da8dce43057913308ae6bf8a00f82103f8ea23f70cda1', 
+  's': '0x7a05d093abc46f947389bfb8f43e66967dc5c1a238624fd9f52e5360a5e66e84', 
+  'blockNumber_int': 3917071,
+  'blockTimestamp_int': datetime.datetime(2017, 6, 23, 1, 18, 52),
+  'value_eth': 0.20317124351371516,
+  'value_int': 203171243513715168,
+  'gasPrice_int': 5000000000,
+  'gas_int': 50000,
+  'input_str': '0x', 
+  'transactionIndex_int': 81}],
+  'transactionsRoot': '0x7e24ebe62f1f913d4e92fcab7475c4e67751fea8041e1314a4216855ff42ce2f', 
+  'uncles': [],
+  'number_int': 3917071,
+  'difficulty_int': 871047975820878,
+  'extraData_str': '0x706f6f6c2e65746866616e732e6f726720284d4e313129', 
+  'timestamp_int': datetime.datetime(2017, 6, 23, 1, 18, 52),
+  'gasLimit_int': 4712394,
+  'gasUsed_int': 4681615,
+  'size_int': 15323,
+  'transactionCount': 82,
+  'txValueSum_int': 275011870412826043342,
+  'totalDifficulty_int': 387548973672392071386}
+*/
+
 CREATE TABLE ethereum_blocks (
-  difficulty VARCHAR(255),
-  difficulty_int BIGINT,
+  difficulty TEXT,
+  difficulty_int NUMERIC,
 
-  extraData VARCHAR(255),
-  extraData_str VARCHAR(255),
+  extraData TEXT,
+  extraData_str TEXT,
 
-  gasLimit VARCHAR(255),
-  gasLimit_int BIGINT,
+  gasLimit TEXT,
+  gasLimit_int NUMERIC,
 
-  gasUsed VARCHAR(255),
-  gasUsed_int BIGINT,
+  gasUsed TEXT,
+  gasUsed_int NUMERIC,
 
-  logsBloom VARCHAR(255),
-  miner VARCHAR(255),
-  mixHash VARCHAR(255),
-  nonce VARCHAR(255),
-  number VARCHAR(255),
-  number_int BIGINT,
+  hash TEXT NOT NULL,
 
-  parentHash VARCHAR(255),
-  receiptsRoot VARCHAR(255),
+  logsBloom TEXT,
+  miner TEXT,
+  mixHash TEXT,
 
-  sha3Uncles VARCHAR(255),
+  nonce TEXT,
+  "number" TEXT NOT NULL,
+  number_int NUMERIC,
 
-  size VARCHAR(255),
-  size_int BIGINT,
+  parentHash TEXT,
+  receiptsRoot TEXT NOT NULL,
 
-  stateRoot VARCHAR(255),
-  timestamp TIMESTAMP,
+  sha3Uncles TEXT NOT NULL,
 
-  totalDifficulty VARCHAR(255),
-  totalDifficult_int BIGINT,
+  size TEXT NOT NULL,
+  size_int NUMERIC,
 
-  transactionCount INT
+  stateRoot TEXT NOT NULL,
+  "timestamp" TIMESTAMP,
+  timestamp_int TIMESTAMP,
+
+  totalDifficulty TEXT NOT NULL,
+  totalDifficulty_int NUMERIC,
+
+  transactionsRoot TEXT NOT NULL,
+  uncles TEXT,
+
+  txvaluesum_int NUMERIC,
+
+  transactionCount NUMERIC
+
 );
 
 CREATE TABLE ethereum_transactions (
-  blockHash VARCHAR(255),
-  blockNumber VARCHAR(255),
-  blockNumber_int BIGINT,
+  blockHash TEXT NOT NULL,
+  blockNumber TEXT NOT NULL,
+  blockNumber_int NUMERIC,
 
-  to_addr VARCHAR(255),
-  from_addr VARCHAR(255),
+  blockTimestamp TIMESTAMP,
+  blockTimestamp_int TIMESTAMP,
 
-  gas VARCHAR(255),
-  gas_int BIGINT,
+  "to" TEXT,
+  "from" TEXT,
 
-  gasPrice VARCHAR(255),
-  gasPrice_int BIGINT,
+  gas TEXT NOT NULL,
+  gas_int NUMERIC,
 
-  hash VARCHAR(255),
+  gasPrice TEXT NOT NULL,
+  gasPrice_int NUMERIC,
 
-  input VARCHAR(255),
-  input_str VARCHAR(255),
+  hash TEXT NOT NULL,
 
-  nonce VARCHAR(255),
+  "input" TEXT,
+  input_str TEXT,
 
-  transactionIndex VARCHAR(255),
-  transactionIndex_int BIGINT,
+  nonce TEXT,
 
-  value VARCHAR(255),
-  value_int BIGINT,
-  value_eth DECIMAL(10,10)
+  transactionIndex TEXT NOT NULL,
+  transactionIndex_int NUMERIC,
+
+  "value" TEXT,
+  value_int NUMERIC,
+  value_eth DECIMAL,
+
+  v TEXT,
+  r TEXT,
+  s TEXT
 );
 
 CREATE TABLE ethereum_parser_states (
-  last_block_number BIGINT
+  last_block_number NUMERIC
 );
